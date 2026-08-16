7547a8ad57667775440cfad34ebf94820ccd2118
AwaneeshSinghBisht <163861104+AwaneeshSinghBisht@users.noreply.github.com>
2026-08-16 22:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+fucntion daysOfAYear(year){
+	if(year % 400 === 0){
+		return 366;
+	}
+	if(year % 4 === 0 && year % 100 !==0){
+		return 366;
+	}
+	return 365;
+}
